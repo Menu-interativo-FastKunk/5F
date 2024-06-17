b978c4625d68421405632f59c2dcbfef6e1c5c2f
Francisco Rafael <franciscocjn06@gmail.com>
2024-06-17 15:05:20 -0300

    Ingredientes.java

--- RESTAURANTE/src/cincoFolhas/Ingredientes.java
+++ RESTAURANTE/src/cincoFolhas/Ingredientes.java
@@ -1,8 +1,9 @@
 package cincoFolhas;
 
 import java.util.*;
-public class Ingredientes extends Produto {
-    static ArrayList<Ingredientes> ingredientes = new ArrayList<Ingredientes>();
+public class Ingredientes {
+    private Scanner input = new Scanner(System.in);
+    ArrayList<Ingredientes> ingredientes = new ArrayList<Ingredientes>();
     String nome;
     double quantidade;
     
@@ -11,7 +12,9 @@ public class Ingredientes extends Produto {
         this.quantidade = quantidade;
     }
 
-    Ingredientes(){}
+    Ingredientes() {
+        listaPadraoIngredientes();
+    }
 
     public Ingredientes novoingrediente(){
         System.out.println("Informe o nome do ingrediente");
@@ -21,7 +24,7 @@ public class Ingredientes extends Produto {
         return new Ingredientes(nome, quantidade);
     }
 
-    public static void exibirIngredientes(){
+    public void exibirIngredientes(){
         System.out.println("------ Ingredientes ------");
         for(int i = 0; i < ingredientes.size(); i++){
             Ingredientes item = ingredientes.get(i);
@@ -38,7 +41,7 @@ public class Ingredientes extends Produto {
         exibirIngredientes();
         System.out.println("Escolha o ingrediente que deseja remover");
         int indice = input.nextInt();
-        if(indice> 0 && indice<=ingredientes.size()){
+        if(indice > 0 && indice <= ingredientes.size()){
             ingredientes.remove((indice - 1));
             System.out.println("Ingrediente removido com sucesso da lista");
         } else{
@@ -47,29 +50,31 @@ public class Ingredientes extends Produto {
     }
 
     public void listaPadraoIngredientes(){
-        ingredientes.add(new Ingredientes("Alface", 10));
-        ingredientes.add(new Ingredientes("Pão", 12));
-        ingredientes.add(new Ingredientes("Ovo", 10));
-        ingredientes.add(new Ingredientes("Farinha de Trigo", 500));
-        ingredientes.add(new Ingredientes("Sal", 10));
-        ingredientes.add(new Ingredientes("Tomate", 10));
-        ingredientes.add(new Ingredientes("Orégano", 10));
-        ingredientes.add(new Ingredientes("Queijo", 10));
-        ingredientes.add(new Ingredientes("Linguiça calabresa", 10));
-        ingredientes.add(new Ingredientes("Azeite", 10));
-        ingredientes.add(new Ingredientes("Cebola", 15));
-        ingredientes.add(new Ingredientes("Alho", 5));
-        ingredientes.add(new Ingredientes("Pepino", 10));
-        ingredientes.add(new Ingredientes("Pimentão", 10));
-        ingredientes.add(new Ingredientes("Frango", 20));
-        ingredientes.add(new Ingredientes("Carne Moída", 25));
-        ingredientes.add(new Ingredientes("Arroz", 100));
-        ingredientes.add(new Ingredientes("Feijão", 80));
-        ingredientes.add(new Ingredientes("Batata", 30));
-        ingredientes.add(new Ingredientes("Cenoura", 20));
-        ingredientes.add(new Ingredientes("Brócolis", 15));
-        ingredientes.add(new Ingredientes("Ervilha", 10));
-        ingredientes.add(new Ingredientes("Milho", 10));
+        if (ingredientes.isEmpty()) {
+            ingredientes.add(new Ingredientes("Alface", 10));
+            ingredientes.add(new Ingredientes("Pão", 12));
+            ingredientes.add(new Ingredientes("Ovo", 10));
+            ingredientes.add(new Ingredientes("Farinha de Trigo", 500));
+            ingredientes.add(new Ingredientes("Sal", 10));
+            ingredientes.add(new Ingredientes("Tomate", 10));
+            ingredientes.add(new Ingredientes("Orégano", 10));
+            ingredientes.add(new Ingredientes("Queijo", 10));
+            ingredientes.add(new Ingredientes("Linguiça calabresa", 10));
+            ingredientes.add(new Ingredientes("Azeite", 10));
+            ingredientes.add(new Ingredientes("Cebola", 15));
+            ingredientes.add(new Ingredientes("Alho", 5));
+            ingredientes.add(new Ingredientes("Pepino", 10));
+            ingredientes.add(new Ingredientes("Pimentão", 10));
+            ingredientes.add(new Ingredientes("Frango", 20));
+            ingredientes.add(new Ingredientes("Carne Moída", 25));
+            ingredientes.add(new Ingredientes("Arroz", 100));
+            ingredientes.add(new Ingredientes("Feijão", 80));
+            ingredientes.add(new Ingredientes("Batata", 30));
+            ingredientes.add(new Ingredientes("Cenoura", 20));
+            ingredientes.add(new Ingredientes("Brócolis", 15));
+            ingredientes.add(new Ingredientes("Ervilha", 10));
+            ingredientes.add(new Ingredientes("Milho", 10));
+        }
     }
 
     @Override
